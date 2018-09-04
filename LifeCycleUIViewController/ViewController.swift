62ef54e302ca4ff3376915c64e3ab495c0e98daa
Denis Zubkov <denzu@macbook-pro-denis.local>
2018-09-04 14:19:45 +0300

    Initial Commit

---
+++ LifeCycleUIViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycleUIViewController
+//
+//  Created by Denis Zubkov on 04/09/2018.
+//  Copyright © 2018 Denis Zubkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
